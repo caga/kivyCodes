a9c81870de629b96b505408927277a25f78231ba
caga <cagataycakir@gmail.com>
2022-08-25 17:08:26 +0300

    today

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+
+# class GridLayoutExample(GridLayout):
+#     pass
 
 
 class BoxLayoutExample(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        b1 = Button(text="CAGATAY")
-        b2 = Button(text="ERTAN")
-        self.add_widget(b1)
-        self.add_widget(b2)
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     b1 = Button(text="A")
+    #     b2 = Button(text="B")
+    #     b3=Button(text="C")
+    #     self.orientation='vertical'
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 
 class MainWidget(Widget):
